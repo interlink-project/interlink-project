2895969c2f6d7108cef10d4dd5ae1a6abcb1c6f5
pablodem <pablodecm@gmail.com>
2022-01-18 09:30:07 +0100

    Create postgres image with multiple database script

---
+++ docker/postgres/create-multiple-postgresql-databases.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -u
+
+# hstore extension needed for database localization https://sqlalchemy-utils.readthedocs.io/en/latest/internationalization.html
+
+function create_user_and_database() {
+	local database=$1
+	echo "  Creating user and database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+		    CREATE USER $database;
+		    CREATE DATABASE $database;
+		    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	EOSQL
+	psql -d $database -c 'create extension hstore;'
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+		create_user_and_database $db
+	done
+	echo "Multiple databases created"
+fi
