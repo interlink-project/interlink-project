940b5373090eae899956001f043eeefc053c9010
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2022-02-07 09:07:52 +0100

    CLIENT SECRET env

--- envs/development/encrypt_secrets.sh
+++
@@ -1,5 +0,0 @@
-gpg --symmetric --cipher-algo AES256 .secrets
-echo "REMEMBER TO SET THIS PASSWORD ON GITHUB SECRETS BEFORE COMMITING"
-
-# to decrypt
-# gpg --quiet --batch --yes --decrypt --passphrase="${{ secrets.SECRETS_ENCRYPTION_KEY }}" --output .secrets .secrets.gpg
