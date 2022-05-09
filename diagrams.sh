0dc7cd9724d8e2d2fd630a4ced67619e36a2dab8
Julen Badiola <badiolajulen@gmail.com>
2022-05-09 08:53:39 +0200

    Googledrive creds

--- diagrams.sh
+++ diagrams.sh
@@ -1,13 +1,10 @@
 cd .. 
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image frontend/docker-compose.yml && mv docker-compose.png frontend.docker-compose.png
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image backend-proxy/docker-compose.yml && mv docker-compose.png coproduction.docker-compose.png
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image backend-coproduction/docker-compose.yml && mv docker-compose.png coproduction.docker-compose.png
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image backend-catalogue/docker-compose.yml && mv docker-compose.png catalogue.docker-compose.png
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image backend-auth/docker-compose.yml && mv docker-compose.png auth.docker-compose.png
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image backend-teammanagement/docker-compose.yml && mv docker-compose.png teammanagement.docker-compose.png
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image frontend/docker-compose.devintegrated.yml && mv docker-compose.png frontend.docker-compose.png
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image backend-coproduction/docker-compose.devintegrated.yml && mv docker-compose.png coproduction.docker-compose.png
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image backend-catalogue/docker-compose.devintegrated.yml && mv docker-compose.png catalogue.docker-compose.png
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image backend-auth/docker-compose.devintegrated.yml && mv docker-compose.png auth.docker-compose.png
 
 # interlinkers
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image interlinker-forum/docker-compose.yml && mv docker-compose.png forum.docker-compose.png
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image interlinker-ceditor/docker-compose.yml && mv docker-compose.png ceditor.docker-compose.png
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image interlinker-googledrive/docker-compose.yml && mv docker-compose.png googledrive.docker-compose.png
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image interlinker-filemanager/docker-compose.yml && mv docker-compose.png filemanager.docker-compose.png
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image interlinker-ceditor/docker-compose.devintegrated.yml && mv docker-compose.png ceditor.docker-compose.png
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image interlinker-googledrive/docker-compose.devintegrated.yml && mv docker-compose.png googledrive.docker-compose.png
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image interlinker-survey/docker-compose.devintegrated.yml && mv docker-compose.png survey.docker-compose.png
--- docs/source/datamodel/index.md
+++ docs/source/datamodel/index.md
@@ -2,3 +2,10 @@
 
 ![Catalogue](catalogue.png)
 ![Coproduction](coproduction.png)
+
+Migrations
+https://stackoverflow.com/questions/24612395/how-do-i-execute-inserts-and-updates-in-an-alembic-upgrade-script
+
+https://github.com/interlink-project/backend-catalogue/tree/master/catalogue/alembic/versions
+https://github.com/interlink-project/backend-catalogue/tree/master/coproduction/alembic/versions
+
