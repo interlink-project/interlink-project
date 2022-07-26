6a395c79d75fac9b4a3fe3e2bc54ded0146696e1
Julen Badiola <badiolajulen@gmail.com>
2022-07-26 09:15:18 +0200

    Setup removed unnecessary

--- envs/development/setup-dremio.py
+++ envs/development/setup-dremio.py
@@ -76,61 +76,6 @@ def getQueryResult(query):
         time.sleep(1)
     return apiGet('job/{id}/results?offset={offset}&limit={limit}'.format(id=jobId, offset=0, limit=100))
 
-MAX_ITERATIONS = 10
-
-def run_queries(queries):
-    results = {}
-    jobs = []
-
-    for query_data in queries:
-        name = query_data.get("name")
-        sql = query_data.get("sql")
-        extract_count = query_data.get("extract_count", False)
-
-        jobs.append({
-            "jobid": querySQL(sql),
-            "name": name,
-            "extract_count": extract_count
-        })
-
-    iteration = 0
-    while len(jobs) > 0 and iteration <= 10:
-        time.sleep(1)
-
-        unfinished_jobs = []
-        for job_data in jobs:
-            jobId = job_data.get("jobid")
-            if queryJobStatus(jobId) == "COMPLETED":
-                # get the result of the query
-                res = apiGet(
-                    'job/{id}/results?offset={offset}&limit={limit}'.format(id=jobId, offset=0, limit=100))
-
-                # process the result
-                if job_data.get("extract_count"):
-                    res = res.get("rows")[0].get('EXPR$0')
-                else:
-                    res = res.get("rows")
-
-                # set the result
-                results[job_data.get("name")] = res
-                print("Query '", job_data.get("name"), "' completed!")
-
-            else:
-                # add to the unfinished jobs object in order to check it in the next iteration
-                unfinished_jobs.append(job_data)
-
-        jobs = unfinished_jobs
-        print(len(unfinished_jobs), "jobs remaining", [i.get("name") for i in unfinished_jobs])
-        iteration += 1
-    return results
-
-def set_list(l, i, v):
-    try:
-        l[i] = v
-    except IndexError:
-        for _ in range(i-len(l)+1):
-            l.append(None)
-        l[i] = v
         
 response = requests.put('{server}/apiv2/bootstrap/firstuser'.format(server=config.dremioServer), headers=config.get_headers(), data=json.dumps({
     "userName": config.userName,
